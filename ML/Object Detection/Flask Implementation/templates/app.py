6611cb30dfec79ec5be004ccb44b66f63a63ce4d
aldebarankwsuperrr <seputargame3576@gmail.com>
2023-06-13 21:21:26 +0700

    mengedit flask app menjadi file python untuk client

--- ML/Object Detection/Flask Implementation/templates/app.py
+++ ML/Object Detection/Flask Implementation/templates/app.py
@@ -3,33 +3,28 @@ import torch
 import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
-from flask import Flask, request, jsonify, render_template
+import requests
 import urllib
-app =Flask(__name__)
 
+# model loading
 classification_model = tf.keras.models.load_model(
-    'model\model_wo_10E.h5',
+    'model/model.h5',
     custom_objects={'KerasLayer':hub.KerasLayer}
 )
-object_detection_model= torch.hub.load('ultralytics/yolov5', 'custom', 'model\model_v3_0_0.pt')
+object_detection_model= torch.hub.load('ultralytics/yolov5', 'custom', 'model/model_v3_1_0.pt')
 
+# list of class
 label_classes = ['freshapples','freshbanana','freshoranges','rottenapples','rottenbanana','rottenoranges']
 label_object_detection = ['apple', 'orange', 'banana']
 
-
+# get image from url
 def url_to_image(url):
-	# download the image, convert it to a NumPy array, and then read
-	# it into OpenCV format
 	resp = urllib.request.urlopen(url)
 	image = np.asarray(bytearray(resp.read()), dtype="uint8")
 	image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-	# return the image
 	return image
 
-@app. route("/predict", methods = ["POST"])
-def predict():
-  data = request.json
-  url = data['url']
+def predict(url):
   image = url_to_image(url)
   result = object_detection_model(image)
   fruit_name  = []
@@ -44,6 +39,7 @@ def predict():
     fruit_name.append(label)
      
     load = image[yA:yB, xA:xB]
+    load = load/255.0
     load = cv2.resize(load, (224,224))
     z = tf.keras.utils.img_to_array(load)
     z = np.expand_dims(z, axis=0)
@@ -52,8 +48,16 @@ def predict():
     index = np.argmax(classes) 
     freshness_level = label_classes[index]
     freshness.append(freshness_level)
-  return jsonify({"name": fruit_name,
-                    "freshness": freshness})   
+  return ({"name": fruit_name, "freshness": freshness})   
     
 if __name__ == "__main__":
-  app.run(debug=True)
+  import sys
+  
+  if len(sys.argv) < 2:
+    print("get output <url>")
+    sys.exit(1)
+  
+  url = sys.argv[1]
+  output = predict(url)
+  
+  print(output)
